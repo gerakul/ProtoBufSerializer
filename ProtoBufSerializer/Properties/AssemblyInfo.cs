f4ec359c484b53dcd3fd24ec8e4b7ce1e574b6fb
Konstantin <gerasimovku@gmail.com>
2015-11-05 19:51:11 +0300

    description properties

--- ProtoBufSerializer/Properties/AssemblyInfo.cs
+++ ProtoBufSerializer/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProtoBufSerializer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Flexibly configurable serialization and deserialization of protobuf format")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("gerakul")]
 [assembly: AssemblyProduct("ProtoBufSerializer")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
